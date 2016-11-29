e2a652316f4c97741a5ada25c59131feac75a456
pedrotst <abreu223@hotmail.com>
2016-11-29 11:08:46 -0200

    Print main variables

--- src/tac_gen.c
+++ src/tac_gen.c
@@ -1,14 +1,17 @@
 #include "tac_gen.h"
 
 void gen_tac(Class *ct, Program *p){
-    Class *main = NULL;
-    HASH_FIND_STR(ct, "Object", main);
+    Class *obj = NULL;
+    Function *main = NULL;
+    HASH_FIND_STR(ct, "Object", obj);
+    HASH_FIND_STR(obj->functions, "main", main);
+
     if(main != NULL){
         printf(".table\n");
         Variable *v;
-        for(v = main->fields; v != NULL; v = v->hh.next){
+        for(v = main->vars; v != NULL; v = v->hh.next){
             if(!strcmp(v->type, "int") || !strcmp(v->type, "bool"))
-                printf("int %s = 0", v->name);
+                printf("int %s = 0\n", v->name);
         }
     }
 }
